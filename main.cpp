b172a073a0640fa188ca122d221f87fd58acda57
SunRain <41245110@qq.com>
2019-05-29 16:37:36 +0800

    merge some origin spek project changes

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     SpekSpectrogram w;
     w.show();
 
-    w.open("/media/wangguojian/MEDIA/music/Soloist.mp3");
+    w.open("/media/wangguojian/MEDIA/music/SH4-RoomofAngel.mp3");
 
     return a.exec();
 }
